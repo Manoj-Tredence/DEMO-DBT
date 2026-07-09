7ef880d69b52887748a9d1a303cdf03ec88c67aa
manoj.snowflake@yahoo.com <manoj.snowflake@yahoo.com>
2026-07-09 12:56:30 +0000

    changes in date

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -2,17 +2,38 @@ WITH CTE AS (
 
     SELECT
         STARTED_AT AS STARTED_AT_RAW,
-        TRY_TO_TIMESTAMP(STARTED_AT) AS STARTED_AT_TS,
-        DATE(TRY_TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-        HOUR(TRY_TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+        TRY_TO_TIMESTAMP(STARTED_AT) AS STARTED_AT_TS
+    FROM {{ source('demo', 'BIKE') }}
+
+),
+
+FINAL AS (
+
+    SELECT
+        STARTED_AT_RAW,
+        STARTED_AT_TS,
+        DATE(STARTED_AT_TS) AS DATE_STARTED_AT,
+        HOUR(STARTED_AT_TS) AS HOUR_STARTED_AT,
+
         CASE
-            WHEN DAYNAME(TRY_TO_TIMESTAMP(STARTED_AT)) IN ('Sat', 'Sun')
+            WHEN DAYNAME(STARTED_AT_TS) IN ('Sat', 'Sun')
                 THEN 'WEEKEND'
             ELSE 'BUSINESSDAY'
-        END AS DAY_TYPE
-    FROM {{ source('demo', 'BIKE') }}
+        END AS DAY_TYPE,
+
+        CASE
+            WHEN MONTH(STARTED_AT_TS) IN (12, 1, 2)
+                THEN 'WINTER'
+            WHEN MONTH(STARTED_AT_TS) IN (3, 4, 5)
+                THEN 'SUMMER'
+            WHEN MONTH(STARTED_AT_TS) IN (6, 7, 8)
+                THEN 'RAINY'
+            ELSE 'AUTUMN'
+        END AS SEASON
+
+    FROM CTE
 
 )
 
 SELECT *
-FROM CTE
+FROM FINAL
